c4901f93a445337e51ea8179c0d756f03d77e19d
anshp2003 <anshpatel20003ap@gmail.com>
2024-02-24 15:40:33 +0530

    new update

---
+++ wb.js
@@ -0,0 +1,12 @@
+const a="home"
+const b="Resources"
+const c="premium"
+const d="about"
+const e="Contact us"
+
+
+    document.getElementById('m1').innerHTML=a;
+    document.getElementById('m2').innerHTML=b;
+    document.getElementById('m3').innerHTML=c;
+    document.getElementById('m4').innerHTML=d;
+    document.getElementById('m5').innerHTML=e;
